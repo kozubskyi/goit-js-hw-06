4400a981e025d6d2229329e3ba9fc819ce0294bc
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2020-12-21 21:15:51 +0200

    hw-6 done

---
+++ js/tasks-1-10.js
@@ -0,0 +1,273 @@
+import users from './users.js'; // users - имя переменной, которое мы сами выбираем
+
+//! Задание 1
+
+// Получить массив имен всех пользователей (поле name).
+
+//* Обычная функция
+// const getUserNames = function(users) {
+//   const userNamesArray = users.map(function (user) {
+//     return user.name
+//   });
+//   return userNamesArray;
+// };
+
+//* Стрелочная функция
+// const getUserNames = users => {
+//   const userNamesArray = users.map(user => {
+//     return user.name
+//   });
+//   return userNamesArray;
+// };
+
+//* Масимально сокращенная запись
+const getUserNames = users => users.map(user => user.name);
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+//! Задание 2
+
+// Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+
+//* Обычная функция
+// const getUsersWithEyeColor = function(users, color) {
+//   const filteredUsers = users.filter(function(user) {
+//     if (color === user.eyeColor) {
+//       return user;
+//     }
+//   });
+//   return filteredUsers;
+// };
+
+//* Стрелочная функция
+// const getUsersWithEyeColor = (users, color) => {
+//   const filteredUsers = users.filter(user => {
+//     if (color === user.eyeColor) {
+//       return user;
+//     }
+//   });
+//   return filteredUsers;
+// };
+
+//* Масимально сокращенная запись
+const getUsersWithEyeColor = (users, color) =>
+  users.filter(user => color === user.eyeColor);
+
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+//! Задание 3
+
+// Получить массив имен пользователей по полу (поле gender).
+
+//* Обычная функция
+// const getUsersWithGender = function (users, gender) {
+//   const filteredUsers = users.filter(function (user) {
+//     if (gender === user.gender) {
+//       return user;
+//     }
+//   });
+//   return filteredUsers;
+// };
+
+//* Стрелочная функция
+// const getUsersWithGender = (users, gender) => {
+//   const filteredUsers = users.filter(user => {
+//     if (gender === user.gender) {
+//       return user
+//     }
+//   })
+//   return filteredUsers
+// }
+
+//* Масимально сокращенная запись
+const getUsersWithGender = (users, gender) =>
+  users.filter(user => gender === user.gender).map(user => user.name);
+
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+
+//! Задание 4
+
+// Получить массив только неактивных пользователей (поле isActive).
+
+//* Стрелочная функция
+// const getInactiveUsers = users => {
+//   const filteredUsers = users.filter(user => {
+//     if (!user.isActive) {
+//       return user;
+//     }
+//   });
+//   return filteredUsers;
+// };
+
+//* Масимально сокращенная запись
+const getInactiveUsers = users => users.filter(user => !user.isActive);
+
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+
+//! Задание 5
+
+// Получить пользоваля (не массив) по email (поле email, он уникальный).
+
+//* Стрелочная функция
+// const getUserWithEmail = (users, email) => {
+//   const neededUser = users.find(user => {
+//     if (email === user.email) {
+//       return user
+//     }
+//   })
+//   return neededUser;
+// };
+
+//* Масимально сокращенная запись
+const getUserWithEmail = (users, email) =>
+  users.find(user => email === user.email);
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+
+//! Задание 6
+
+// Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
+
+//* Стрелочная функция
+// const getUsersWithAge = (users, min, max) => {
+//   const filteredUsers = users.filter(user => {
+//     if (user.age >= min && user.age < max) {
+//       return user;
+//     }
+//   });
+//   return filteredUsers;
+// };
+
+//* Масимально сокращенная запись
+const getUsersWithAge = (users, min, max) =>
+  users.filter(user => user.age >= min && user.age < max);
+
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+
+//! Задание 7
+
+// Получить общую сумму баланса (поле balance) всех пользователей.
+
+//* Стрелочная функция через метод .forEach()
+// const calculateTotalBalance = users => {
+//   let totalBalance = 0;
+//   users.forEach(user => {
+//     return (totalBalance += user.balance);
+//   });
+//   return totalBalance;
+// };
+
+//* Стрелочная функция через метод .reduce()
+// const calculateTotalBalance = users => {
+//   let totalBalance = users.reduce((acc, user) => {
+//     return acc + user.balance
+//   }, 0)
+//   return totalBalance
+// };
+
+//* Масимально сокращенная запись
+const calculateTotalBalance = users =>
+  users.reduce((totalBalance, user) => totalBalance + user.balance, 0);
+
+console.log(calculateTotalBalance(users)); // 20916
+
+//! Задание 8
+
+// Массив имен всех пользователей у которых есть друг с указанным именем.
+
+//* Сделал через методы .reduce() и .includes()
+// const getUsersWithFriend = (users, friendName) => {
+//   const userNames = users.reduce((acc, user) => {
+//     if (user.friends.includes(friendName)) {
+//       acc.push(user.name);
+//     }
+//     return acc;
+//   }, []);
+//   return userNames
+// };
+
+//* Сделал через методы .reduce() и .some()
+// const getUsersWithFriend = (users, friendName) => {
+//   const userNames = users.reduce((acc, user) => {
+//     if (user.friends.some(userFriend => friendName === userFriend)) {
+//       acc.push(user.name);
+//     }
+//     return acc;
+//   }, []);
+//   return userNames;
+// };
+
+//*Максимально короткая запись
+const getUsersWithFriend = (users, friendName) =>
+  users.reduce(
+    (array, user) =>
+      user.friends.includes(friendName) ? [...array, user.name] : array,
+    // Не совсем понимаю зачем нужна запись [...array, user.name] поскольку мы уже вроде как создали массив ниже при помощи .reduce()
+    [],
+  );
+
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+
+//! Задание 9
+
+// Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
+
+//*Максимально короткая запись
+const getNamesSortedByFriendsCount = users =>
+  [...users]
+    .sort((prev, next) => prev.friends.length - next.friends.length)
+    .map(user => user.name);
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+//! Задание 10
+
+// Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+
+// 1. Перебрать массив users
+// 2. Перебрать массив skills каждого обьекта
+// 3. Если элемент массива skills не совпадает ни с одним элементом из массива skillsArray, то запушить его в skillsArray
+// 4. Отсортировать с помощью .sort()
+
+//* Сделал через методы .forEach(), .includes(), .push(), .sort()
+// const getSortedUniqueSkills = users => {
+//   const skillsArray = [];
+
+//   users.forEach(user => {
+//     user.skills.forEach(skill => {
+//       if (skillsArray.includes(skill)) {
+//         return;
+//       }
+//       skillsArray.push(skill);
+//       return skill;
+//     });
+//     return user;
+//   });
+
+//   skillsArray.sort();
+
+//   return skillsArray;
+// };
+
+const getSortedUniqueSkills = users => {
+  const skillsArray = [];
+
+  users.forEach(user =>
+    user.skills.forEach(skill =>
+      skillsArray.includes(skill) ? null : skillsArray.push(skill),
+    ),
+  );
+  skillsArray.sort();
+
+  return skillsArray;
+};
+
+console.log(getSortedUniqueSkills(users));
+// (19) [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
---
+++ js/test.js
@@ -0,0 +1,135 @@
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
+
+// console.table('players:', players);
+
+// /*
+//  * Получаем массив имен всех игроков
+//  */
+
+// // Через обычный for
+// const playerNames = [];
+// for (const player of players) {
+//   playerNames.push(player.name);
+// }
+// // Через метод .map()
+// const playerNamesMap = players.map(function (player) {
+//   return player.name;
+// });
+// console.log(playerNames);
+// console.log(playerNamesMap);
+
+// /*
+//  * Увеличиваем кол-во поинтов каждого игрока
+//  */
+
+// // Через обычный for
+// console.log('players:', players);
+
+// const updatedPlayers = [];
+// for (const player of players) {
+//   updatedPlayers.push({
+//     ...player,
+//     points: player.points + player.points * 0.1,
+//   });
+
+// }
+
+// console.log('updatedPlayers:', updatedPlayers);
+// console.log('players:', players);
+
+// // Через метод .map()
+// const updatedPlayersMap = players.map(function (player) {
+//   return { ...player, points: player.points + player.points * 0.1 }; // При { points: player.points * 1.1 } после запятой указывается очень много цифр...
+// });
+
+// console.table(updatedPlayers === players); // false
+// console.table(updatedPlayers[0] === players[0]); // true
+// console.table(updatedPlayersMap === updatedPlayers); // false
+// console.table(updatedPlayersMap[0] === players[0]); // false
+// console.table(updatedPlayersMap2[0] === updatedPlayersMap[0]); // true
+
+// /*
+//  * Увеличиваем кол-во часов игрока по id
+//  */
+
+// // Через метод .map()
+// const playerIdToUpdate = 'player-3';
+// const updatedPlayersId = players.map(function (player) {
+
+//   if (player.id === playerIdToUpdate) {
+//     return {
+//       ...player, timePlayed: player.timePlayed + 50
+//     }
+//   }
+
+//   return player;
+// });
+
+// console.table(updatedPlayersId);
+
+// /*
+//  * map своими руками
+//  */
+
+// const nubmers = [5, 10, 15, 20, 25];
+
+// // - создает и возвращает новый массив
+// // - перебирает оригинальный массив
+// // // - вызывает колбек для каждого элемента и кидает туда аргументы
+// //?  зачем колбек?
+// // // - записывает результат вызова функции в новый массив
+// // - записывает значение элементов в новый массив
+
+// const map = function (array) {
+//   const newArray = [];
+
+//   for (let i = 0; i < array.length; i++) {
+//     newArray.push(array[i] * 2);
+//     console.log(array[i]);
+//     console.log(i);
+//     console.log(array);
+//   }
+
+//   return newArray;
+// };
+
+// console.log(map(nubmers));
+
+//! Тренировка
+
+// const salary = { mango: 100, poly: 50, ajax: 150 };
+// const values = Object.values(salary);
+// console.log(values);
+// const totalSalary = values.reduce(((acc, value) => acc + value), 0)
+// console.log(totalSalary);
+
+// const numbers = [1, 2, 3];
+// console.log(numbers);
+// numbers.forEach((number, index) => numbers[index] += 10)
+// console.log(numbers);
+
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const filteredNumbers = numbers.filter(el => el < 5)
+// console.log(filteredNumbers);
+
+// const add = function () { console.log(arguments) };
+// add(1, 2, 3, 4, 5); // Arguments(5) [1, 2, 3, 4, 5, callee: (...), Symbol(Symbol.iterator): f]
+// const add2 = () => { console.log(arguments) };
+// add2(1, 2, 3, 4, 5); // Uncaught ReferenceError: arguments is not defined
+
+// const cart = [ { label: 'Apples', price: 100, quantity: 2 }, { label: 'Bananas', price: 120, quantity: 3 }, { label: 'Lemons', price: 70, quantity: 4 }, ];
+// const countTotalAmount = function (array) {
+//   let totalAmount = array.reduce((acc, element) => {
+//     return acc + element.price * element.quantity;
+//   }, 0);
+//   return totalAmount;
+// };
+// Ниже максимально сокразенная запись
+// const countTotalAmount = array => array.reduce((acc, element) => acc + element.price * element.quantity, 0);
+// console.log(countTotalAmount(cart));
